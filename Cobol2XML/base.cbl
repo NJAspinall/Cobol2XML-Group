66ecb31e08f960a69274cd082869d2b4af6f3a17
NJAspinall <97522482+NJAspinall@users.noreply.github.com>
2022-05-03 14:09:46 +0100

    Add files via upload

---
+++ Cobol2XML/base.cbl
@@ -0,0 +1,16 @@
+identification division.
+
+program-id.  base_jb.
+
+
+date-written.  7-jul-1995 - mb.
+
+data division.
+working-storage section.
+
+procedure division.
+
+my-function.
+
+
+***--- This is a comment
