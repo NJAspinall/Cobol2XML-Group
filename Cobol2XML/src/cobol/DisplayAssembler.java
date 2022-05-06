5f260ecfba102dcf0fcf29e51a454c75877f2a93
NJAspinall <97522482+NJAspinall@users.noreply.github.com>
2022-05-06 13:19:58 +0100

    Add files via upload

---
+++ Cobol2XML/src/cobol/DisplayAssembler.java
@@ -0,0 +1,36 @@
+package cobol;
+
+import parse.Assembler;
+import parse.Assembly;
+import parse.tokens.Token;
+
+public class DisplayAssembler extends Assembler {
+
+	@Override
+	public void workOn(Assembly a) {
+		
+		System.out.println("New Display...");
+		
+		Cobol c = new Cobol();
+		Token t;
+		
+		//dispose of 'display'
+		t = (Token) a.pop();
+		System.out.println("Initial T : " +t.sval());
+		
+		//get the strings and variables to display
+		String displayString = " ";
+		while(a.stackIsEmpty() == false) {
+			t = (Token) a.pop();
+			displayString = t.sval() + displayString;
+			displayString = " " + displayString;
+			System.out.println("display T : " +t.sval());
+		}
+		
+		if(!displayString.isBlank()) {
+			c.setDisplayString(displayString);
+			a.setTarget(c);
+		}
+	}
+
+}
