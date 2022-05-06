d15f1d474704d00c49ed7096e3ce879d0c4a328a
danroberts108 <d.roberts15@edu.salford.ac.uk>
2022-05-06 13:27:54 +0100

    Removed unneeded tests

--- Cobol2XML/src/jUnit/DisplayTest.java
+++ Cobol2XML/src/jUnit/DisplayTest.java
@@ -2,13 +2,38 @@ package jUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+
+import cobol.Cobol;
+import cobol.CobolParser;
+import parse.Assembly;
+import parse.Parser;
+import parse.tokens.Literal;
+import parse.tokens.TokenAssembly;
+import parse.tokens.Tokenizer;
 
 class DisplayTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Tokenizer t = CobolParser.tokenizer();
+		Parser p = CobolParser.start();
+		t.setString("display \"Decimal  value: \" entry_char");
+		Assembly in = new TokenAssembly(t);
+		Assembly out = p.bestMatch(in);
+		
+		assertFalse(out.stackIsEmpty());
+		
+		ArrayList<Parser> visitedList = new ArrayList<Parser>();
+		Literal literal = new Literal("display \"Decimal  value: \" entry_char");
+		String string = literal.unvisitedString(visitedList);
+		assertEquals(string, "display \"Decimal  value: \" entry_char");
+		
+		Cobol c = (Cobol) out.getTarget();
+		assertNotEquals(null, c.getDisplayString());
+		assertEquals("procedure", c.getDivisionName());
 	}
 
 }
